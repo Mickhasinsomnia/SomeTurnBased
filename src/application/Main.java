09e5afe31677713fb8e3e2974252bfe44148b2d6
Tananan Narkchuay <zackalocloud@gmail.com>
2025-02-20 20:32:49 +0700

    fix

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.ArrayList;
+
 import grid.LevelSelect;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -13,49 +15,48 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
 public class Main extends Application {
-   
-    private Stage primary;
-    public static VBox menu;
-    
-    @Override
-    public void start(Stage primaryStage) {
-       
-    	Button startButton=setButton(1);
-    	Button startButton2=setButton(2);
-        primary=primaryStage;
-        
-        VBox root = new VBox(20); 
-        root.setAlignment(Pos.CENTER); 
-        root.getChildren().addAll(startButton, startButton2);
-        Main.menu=root;
-       
-       
-       
-       Scene scene = new Scene(root, 900, 700); 
-       primaryStage.setTitle("Start Game");
-       primaryStage.setScene(scene);
-       primaryStage.show();
-   
-       
-      
-       
-      primaryStage.setResizable(false);
-    }
 
-    
-    public static void main(String[] args) {
+	private Stage primary;
+	public static VBox menu;
 
-    	launch(args);
-    }
-    
-    private Button setButton(int level) {
-    	Button startButton = new Button("Level "+level);
-        startButton.setFont(Font.font("Arial", FontWeight.BOLD, 24)); 
-        startButton.setMinWidth(200);  
-        startButton.setMinHeight(50);
-        startButton.setOnMouseClicked(e->{
-     	   LevelSelect.Level(level, primary);
-        });
-    	return startButton;
-    }
+	@Override
+	public void start(Stage primaryStage) {
+
+		
+		ArrayList<Button>buttonList=new ArrayList<>();
+		for(int i=1;i<=10;i++) {
+			buttonList.add(setButton(i));
+		}
+		
+		
+		primary = primaryStage;
+
+		VBox root = new VBox(20);
+		root.setAlignment(Pos.CENTER);
+		root.getChildren().addAll(buttonList);
+		Main.menu = root;
+
+		Scene scene = new Scene(root, 900, 700);
+		primaryStage.setTitle("Start Game");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+		primaryStage.setResizable(false);
+	}
+
+	public static void main(String[] args) {
+
+		launch(args);
+	}
+
+	private Button setButton(int level) {
+		Button startButton = new Button("Level " + level);
+		startButton.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+		startButton.setMinWidth(200);
+		startButton.setMinHeight(50);
+		startButton.setOnMouseClicked(e -> {
+			LevelSelect.Level(level, primary);
+		});
+		return startButton;
+	}
 }
